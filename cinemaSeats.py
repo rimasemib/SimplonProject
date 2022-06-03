9ed8b5ea650cc6a76d2ecac658dfd028981f1890
rimasemib <85959976+rimasemib@users.noreply.github.com>
2022-06-03 19:31:21 +0200

    Update cinemaSeats.py

--- cinemaSeats.py
+++ cinemaSeats.py
@@ -1,5 +1,3 @@
-
-
 x = 10
 Booked_seat = 0
 Row = 8
@@ -30,10 +28,9 @@ while x != 0:
     print('0 for Exit')
     x = int(input('Select Option - '))
     if x == 1:
-        if Seats == 9:
-            for seat in range(Seats):
-                print(seat, end='  ')
-            print(Seats)
+        for seat in range(Seats):
+            print(seat, end='  ')
+        print(Seats)
 
         if Seats == 9:
             for num in table_of_chart.keys():
@@ -41,22 +38,7 @@ while x != 0:
                 for no in table_of_chart[num].values():
                     print(no, end='  ')
                 print()
-        else:
-            count_num = 0
-            for num in table_of_chart.keys():
-                if int(list(table_of_chart.keys())[count_num]) < 9:
-                    print(int(num) + 1, end='  ')
-                else:
-                    print(int(num) + 1, end=' ')
-                count_key = 0
-                for no in table_of_chart[num].values():
-                    if int(list(table_of_chart[num].keys())[count_key]) <= 10:
-                        print(no, end='  ')
-                    else:
-                        print(no, end='  ')
-                    count_key += 1
-                count_num += 1
-                print()
+
         print('Vacant Seats = ', Total_seat - Booked_seat)
         print()
 
@@ -64,7 +46,7 @@ while x != 0:
         Row_number = int(input('Enter Row Number - \n'))
         Column_number = int(input('Enter Column Number - \n'))
         conform = input('yes for booking and no for Stop booking - ')
-        person_detail = {}
+
         if conform == 'yes':
             table_of_chart[str(Row_number - 1)][str(Column_number)] = 'B'
             Booked_seat += 1
