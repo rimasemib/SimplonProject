f1fd0f78d0bff78d4841dc43610b5042b8580caf
rimasemib <85959976+rimasemib@users.noreply.github.com>
2022-06-07 18:27:01 +0200

    Update cinemaSeats.py

--- cinemaSeats.py
+++ cinemaSeats.py
@@ -1,22 +1,13 @@
-x = 10
-Booked_seat = 0
-Row = 8
-Seats = 9
-Total_seat = Row * Seats
+x = 3
 
-class chart:
+seats = []
+seats.append(["vacant", "booked", "vacant", "vacant", "booked", "vacant", "vacant", "vacant"])
+seats.append(["vacant", "vacant", "booked", "vacant", "vacant", "booked", "vacant", "vacant"])
+seats.append(["booked", "vacant", "vacant", "booked", "vacant", "vacant", "vacant", "vacant"])
+seats.append(["vacant", "vacant", "vacant", "vacant", "vacant", "vacant", "booked", "booked"])
+seats.append(["booked", "vacant", "booked", "vacant", "vacant", "vacant", "vacant", "vacant"])
+seats.append(["vacant", "booked", "vacant", "booked", "vacant", "vacant", "vacant", "vacant"])
 
-    def chart_maker():
-        seats_chart = {}
-        for i in range(Row):
-            seats_in_row = {}
-            for j in range(Seats):
-                seats_in_row[str(j + 1)] = 'S'
-            seats_chart[str(i)] = seats_in_row
-        return seats_chart
-
-class_call = chart
-table_of_chart = class_call.chart_maker()
 
 while x != 0:
     print('1 for Show the seats')
@@ -24,38 +15,21 @@ while x != 0:
 
     x = int(input('Select Option - '))
     if x == 1:
-        for seat in range(Seats):
-            print(seat, end='  ')
-        print(Seats)
+        print("")
+        print("======================================")
+        print("")
+        for row in seats:
+            print(row)
+        print("")
+        print("======================================")
 
-        if Seats == 9:
-            for num in table_of_chart.keys():
-                print(int(num) + 1, end='  ')
-                for no in table_of_chart[num].values():
-                    print(no, end='  ')
-                print()
-
-        print('Vacant Seats = ', Total_seat - Booked_seat)
         print()
 
     elif x == 2:
-        Row_number = int(input('Enter Row Number - \n'))
-        Column_number = int(input('Enter Column Number - \n'))
-        conform = input('yes for booking and no for Stop booking - ')
+        row = int(input("Enter a row number (between 0 and 5)"))
+        column = int(input("Enter a column number (between 0 and 7)"))
 
-        if conform == 'yes':
-            table_of_chart[str(Row_number - 1)][str(Column_number)] = 'B'
-            Booked_seat += 1
-
-        elif conform == 'no':
-
-            print('This seat already booked by some one')
+        if seats[row][column] == "booked":
+            print("This seat is already booked.")
         else:
-            print('***  Invalid Input  ***')
-    else:
-        print('***  Invalid Input  ***')
-        print()
-
-
-
-
+            print("This seat is vacant.")
